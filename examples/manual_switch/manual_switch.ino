7f2992779e96d134de9a08e54cd9c929f7a3e29b
Luca Morandini <luca.morandini98@gmail.com>
2017-08-16 19:28:11 +0200

    Updated manual switch example

--- examples/manual_switch/manual_switch.ino
+++ examples/manual_switch/manual_switch.ino
@@ -5,27 +5,30 @@ DST dst;
 dst_limit_t beginning;
 dst_limit_t end;
 
-bool done = false;
+int one_hour = 3600 * 1000;
+uint32_t last_check;
 
 void setup() {
+  Serial.begin(9600);
+
   beginning.hour = 2;
-  beginning.day = 3;
-  beginning.month = 2;
+  beginning.day = 3; // tuesday
+  beginning.month = 2; // february
   beginning.occurrence = 2;
 
   end.hour = 3;
-  end.day = 4;
-  end.month = 10;
+  end.day = 4; // wednesday
+  end.month = 10; // october
   end.occurrence = 2;
 
   dst.begin(beginning, end, 1);
 }
 
 void loop() {
-  delay(3000);
-  if (!done) {
-    done = true;
+  if (millis() - last_check > one_hour || last_check == 0) {
+    last_check = millis();
 
-    dst.check();
+    bool enabled = dst.check();
+    Serial.printlnf("DST: %s", enabled ? "enabled" : "disabled");
   }
 }
