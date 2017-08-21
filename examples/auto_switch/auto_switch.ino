2ad067e3595e79d7864bb143e538c6893a3e26a7
Luca Morandini <luca.morandini98@gmail.com>
2017-08-21 19:44:29 +0200

    Added days and months enumerators

--- examples/auto_switch/auto_switch.ino
+++ examples/auto_switch/auto_switch.ino
@@ -9,13 +9,17 @@ void setup() {
   Serial.begin(9600);
 
   beginning.hour = 2;
-  beginning.day = 3; // tuesday
-  beginning.month = 2; // february
+  //beginning.day = 3; // tuesday
+  beginning.day = DST::days::tue;
+  //beginning.month = 2; // february
+  beginning.month = DST::months::feb;
   beginning.occurrence = 2;
 
   end.hour = 3;
-  end.day = 4; // wednesday
-  end.month = 10; // october
+  //end.day = 4; // wednesday
+  end.day = DST::days::wed;
+  //end.month = 10; // october
+  end.month = DST::months::oct;
   end.occurrence = 2;
 
   dst.begin(beginning, end, 1);
--- src/particle-dst.h
+++ src/particle-dst.h
@@ -16,6 +16,10 @@ class DST
     void begin(dst_limit_t beginning_limit, dst_limit_t end_limit, int offset);
     bool check();
     void automatic(bool enable);
+    
+    // days and months enumerators
+    enum days { sun = 1, mon = 2, tue = 3, wed = 4, thu = 5, fri = 6, sat = 7 };
+    enum months { jan = 1, feb = 2, mar = 3, apr = 4, may = 5, jun = 6, jul = 7, aug = 8, sep = 9, oct = 10, nov = 11, dec = 12 };
   private:
     dst_limit_t beginning_l;
     dst_limit_t end_l;
